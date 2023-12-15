52b03e5384d87e6ce4714b6066502f1bf5291f0a
Marcos Campos <contato.marcoscampos@gmail.com>
2023-12-15 20:57:10 -0300

    Enviando dados para o MS de usuários

--- static/js/signup.js
+++ static/js/signup.js
@@ -2,7 +2,7 @@ $(function(){
     $("#btn-register").on("click", registerUser)
 
     function registerUser(event){
-        var url = "https://httpbin.org/anything";
+        var url = "http://localhost:8000/api/users";
         
         event.preventDefault();
 
@@ -11,12 +11,20 @@ $(function(){
             email: $("#user-email-field").val()
         }
 
-        $.post(url, user)
-            .done(function(data) {
-                alert("Resposta do servidor: " + data.url)
-            })
-            .fail(function(error){
-                alert("Erro na chamada: " + error)
-            })
+        var body = JSON.stringify(user)
+
+        $.post({
+            url: url,
+            data: body,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            success: function(response){
+                alert("Resposta do servidor: " + response.uri)
+            },
+            error: function(xhr, status, err){
+                alert("Erro na chamada: " + err.responseText())
+            }
+        })
     }
 });
